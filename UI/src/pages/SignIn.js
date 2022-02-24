f36fb4aee8f89d90e3ecee32567e6872aa9bd67d
melhamin <mo.elhamin@gmail.com>
2022-02-24 17:06:22 +0300

    implement test_cases 3 from the sheet

--- UI/src/pages/SignIn.js
+++ UI/src/pages/SignIn.js
@@ -1,12 +1,10 @@
-import React, { useState, useCallback, useEffect } from 'react';
-
-import '../styles/SignIn.css';
-import { getAuth, signInWithEmailAndPassword, signInWithPopup, FacebookAuthProvider } from 'firebase/auth';
+import { FacebookAuthProvider, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import React, { useCallback, useEffect, useState } from "react";
 import { Spinner } from "react-activity";
 import "react-activity/dist/library.css";
+import "../styles/SignIn.css";
 
 export default function SignIn() {
-
   //inputs
   const [emailPhone, setEmailPhone] = useState("");
   const [password, setPassword] = useState("");
@@ -26,26 +24,27 @@ export default function SignIn() {
   //loading
   const [loading, setLoading] = useState(false);
 
-  const rememberUser = useCallback((remember) => {
-    if (remember) {
-      try {
-        localStorage.setItem('Username', emailPhone);
-        localStorage.setItem('Password', password);
-      } catch (error) {
-      }
-    } else
-      try {
-        localStorage.removeItem('Username');
-        localStorage.removeItem('Password');
-      } catch (error) {
-      }
-  }, [emailPhone, password]);
+  const rememberUser = useCallback(
+    (remember) => {
+      if (remember) {
+        try {
+          localStorage.setItem("Username", emailPhone);
+          localStorage.setItem("Password", password);
+        } catch (error) {}
+      } else
+        try {
+          localStorage.removeItem("Username");
+          localStorage.removeItem("Password");
+        } catch (error) {}
+    },
+    [emailPhone, password]
+  );
 
   const handleShowPassword = (e) => {
     e.preventDefault();
     setShowPassword(!showPassword);
     document.getElementById("password").focus();
-  }
+  };
 
   const handleBlur = (e) => {
     const buttonTarget = e.relatedTarget && e.relatedTarget.id === "show-password";
@@ -53,34 +52,27 @@ export default function SignIn() {
       setShowPassword(false);
       setTogglePasswordButton(false);
     }
-  }
-
-
+  };
 
   const getRememberedUsername = () => {
     try {
-      const username = localStorage.getItem('Username');
-      if (username !== null)
-        return username;
-      else
-        return "";
+      const username = localStorage.getItem("Username");
+      if (username !== null) return username;
+      else return "";
     } catch (error) {
       return "";
     }
-  }
-
+  };
 
   const getRememberedPassword = () => {
     try {
-      const password = localStorage.getItem('Password');
-      if (password !== null)
-        return password;
-      else
-        return "";
+      const password = localStorage.getItem("Password");
+      if (password !== null) return password;
+      else return "";
     } catch (error) {
       return "";
     }
-  }
+  };
 
   const signInWithFacebook = () => {
     const provider = new FacebookAuthProvider();
@@ -92,53 +84,48 @@ export default function SignIn() {
       .catch((error) => {
         const errorCode = error.code;
         console.log("errorCode", errorCode);
-      })
-  }
+      });
+  };
 
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      setLoading(true);
 
+      const emailError = emailPhone.length < 5;
+      const passwordError = password.length < 4 || password.length > 60;
+      const remember = e.target.rememberMe.checked;
+      rememberUser(remember);
 
-  const handleSubmit = useCallback(async (e) => {
-    e.preventDefault();
-    setLoading(true);
+      if (!emailError && !passwordError) {
+        console.log("emailPhone: " + emailPhone);
+        console.log("password: " + password);
+        setErrorEmail(false);
+        setErrorPassword(false);
 
-    const emailError = emailPhone.length < 5;
-    const passwordError = password.length < 4 || password.length > 60;
-    const remember = e.target.rememberMe.checked;
-    rememberUser(remember);
+        try {
+          await signInWithEmailAndPassword(auth, emailPhone, password);
+          window.location.href = "/home";
+        } catch (err) {
+          const errorCode = err.code;
 
-
-    if (!emailError && !passwordError) {
-      console.log("emailPhone: " + emailPhone);
-      console.log("password: " + password);
-      setErrorEmail(false);
-      setErrorPassword(false);
-
-      try {
-        await signInWithEmailAndPassword(auth, emailPhone, password);
-        window.location.href = "/home";
-      } catch (err) {
-
-        const errorCode = err.code;
-
-        console.log("errorCode: " + errorCode);
-        if (errorCode.includes("wrong-password")) {
-          setValidMessage("Parola yanlış. Lütfen yeniden deneyin ya da parolanızı sıfırlayın");
+          console.log("errorCode: " + errorCode);
+          if (errorCode.includes("wrong-password")) {
+            setValidMessage("Parola yanlış. Lütfen yeniden deneyin ya da parolanızı sıfırlayın");
+          } else if (errorCode.includes("invalid-email") || errorCode.includes("user-not-found")) {
+            setValidMessage("Bu e-posta adresi ile bağlantılı bir hesap bulamadık. Lütfen yeniden deneyin ya da yeni bir hesap oluşturun.");
+          }
+          setLoading(false);
         }
-
-        else if (errorCode.includes("invalid-email") || errorCode.includes("user-not-found")) {
-          setValidMessage("Bu e-posta adresi ile bağlantılı bir hesap bulamadık. Lütfen yeniden deneyin ya da yeni bir hesap oluşturun.");
-        }
-        setLoading(false);
-
+      } else {
+        setErrorEmail(emailError);
+        setErrorPassword(passwordError);
       }
-    }
-    else {
-      setErrorEmail(emailError);
-      setErrorPassword(passwordError);
-    }
 
-    setLoading(false);
-  }, [emailPhone, password, auth, rememberUser]);
+      setLoading(false);
+    },
+    [emailPhone, password, auth, rememberUser]
+  );
 
   useEffect(() => {
     setEmailPhone(getRememberedUsername());
@@ -154,51 +141,87 @@ export default function SignIn() {
 
         <div className="loginForm">
           <h1>Oturum Aç</h1>
-          {validMessage && <div className='messageContainer'>{validMessage}</div>}
-          <form autoComplete='off' onSubmit={handleSubmit}>
+          {validMessage && <div className="messageContainer">{validMessage}</div>}
+          <form autoComplete="off" onSubmit={handleSubmit}>
             <div className="inputGroup">
-              <div className="inputContainer" style={{ borderBottom: errorEmail ? '3px solid #e87c03' : 'none' }}>
-                <input className="form-input" id="email_phone" type="text" autoComplete='off' required value={emailPhone} onChange={e => setEmailPhone(e.target.value)} noValidate="" />
-                <label className="form-label" htmlFor="email_phone">E-posta veya telefon numarası</label>
+              <div className="inputContainer" style={{ borderBottom: errorEmail ? "3px solid #e87c03" : "none" }}>
+                <input
+                  className="form-input"
+                  id="email_phone"
+                  type="text"
+                  autoComplete="off"
+                  required
+                  value={emailPhone}
+                  onChange={(e) => setEmailPhone(e.target.value)}
+                  noValidate=""
+                />
+                <label className="form-label" htmlFor="email_phone">
+                  E-posta veya telefon numarası
+                </label>
               </div>
               {errorEmail ? <div className="error">Lütfen geçerli bir telefon numarası veya e-posta adresi girin.</div> : undefined}
             </div>
             <div className="inputGroup">
-              <div className="inputContainer" id="passwordContainer" style={{ borderBottom: errorPassword ? '3px solid #e87c03' : 'none' }}>
-                <input className="form-input" id="password" type={showPassword ? "text" : "password"} autoComplete='off' required noValidate=""
-                  onFocus={() => setTogglePasswordButton(true)} onBlur={handleBlur} value={password} onChange={e => setPassword(e.target.value)} />
-                <label className="form-label" htmlFor="password">Parola</label>
-                <button id="show-password" style={{ display: togglePasswordButton ? 'block' : 'none' }}
-                  onClick={(handleShowPassword)} className="show-password">{showPassword ? "GİZLE" : "GÖSTER"}</button>
+              <div className="inputContainer" id="passwordContainer" style={{ borderBottom: errorPassword ? "3px solid #e87c03" : "none" }}>
+                <input
+                  className="form-input"
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  autoComplete="off"
+                  required
+                  noValidate=""
+                  onFocus={() => setTogglePasswordButton(true)}
+                  onBlur={handleBlur}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label className="form-label" htmlFor="password">
+                  Parola
+                </label>
+                <button
+                  id="show-password"
+                  style={{ display: togglePasswordButton ? "block" : "none" }}
+                  onClick={handleShowPassword}
+                  className="show-password"
+                >
+                  {showPassword ? "GİZLE" : "GÖSTER"}
+                </button>
               </div>
               {errorPassword ? <div className="error">Parola 4 ile 60 karakter olmalıdır.</div> : undefined}
             </div>
-            <button disabled={loading} type="submit" formNoValidate>{loading ? <Spinner style={{ margin: 'auto' }} /> : "Oturum Aç"}</button>
+            <button id="login-submit" disabled={loading} type="submit" formNoValidate>
+              {loading ? <Spinner style={{ margin: "auto" }} /> : "Oturum Aç"}
+            </button>
             <div className="infoContainer">
-              <div className='rememberMe'>
-                <input id="rememberMe" type="checkbox" />Beni Hatırla
+              <div className="rememberMe">
+                <input id="rememberMe" type="checkbox" />
+                Beni Hatırla
                 <button onClick={handleSubmit}></button>
-                <div>
-
-                </div>
+                <div></div>
               </div>
-              <a className="help" href="/">Yardım ister misiniz?</a>
+              <a className="help" href="/">
+                Yardım ister misiniz?
+              </a>
             </div>
           </form>
           <button onClick={signInWithFacebook} className="fb-block">
             <img className="fb-logo" src="https://assets.nflxext.com/ffe/siteui/login/images/FB-f-Logo__blue_57.png" alt="facebook-logo" />
-            <div className='fb-text'>Facebook ile Oturum Aç</div>
+            <div className="fb-text">Facebook ile Oturum Aç</div>
           </button>
-          <div className='loginSignUpNow'>
-            Netflix'e katılmak ister misiniz? <a className="signup-text" href="/">Şimdi kaydolun.</a>
+          <div className="loginSignUpNow">
+            Netflix'e katılmak ister misiniz?{" "}
+            <a className="signup-text" href="/">
+              Şimdi kaydolun.
+            </a>
           </div>
-          <div className='recaptcha'>
+          <div className="recaptcha">
             <div>Bu sayfa robot olmadığınızı kanıtlamak için Google reCAPTCHA tarafından korunuyor.</div>
-            <a className="learn-more" href="/">Daha fazlasını öğrenin.</a>
+            <a className="learn-more" href="/">
+              Daha fazlasını öğrenin.
+            </a>
           </div>
         </div>
       </div>
     </>
   );
-
 }
