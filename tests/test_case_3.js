75008d0a79e6a74e7d6e75c96f506b6119831812
melhamin <mo.elhamin@gmail.com>
2022-02-26 12:23:15 +0300

    add tests for remember me

--- tests/test_case_3.js
+++ tests/test_case_3.js
@@ -8,30 +8,46 @@ describe("TEST SUITE 3", () => {
   let driver;
 
   beforeEach(async () => {
-    driver = await new Builder().forBrowser("chrome").build();
+    driver = await buildDriver();
   });
 
   afterEach(async () => {
     await driver.quit();
   });
 
-  it("Verifies the login page by pressing 'Back button' of the browser. It should not allow the user to enter into the system once they log out.", async () => {
-    await login(driver);
-    await logout(driver);
-    await driver.navigate().back();
-    const title = await driver.getTitle();
-    expect(title).to.equal("Netflix Login");
-  });
+  // it("Verifies the login page by pressing 'Back button' of the browser. It should not allow the user to enter into the system once they log out.", async () => {
+  //   await login(driver);
+  //   await logout(driver);
+  //   await driver.navigate().back();
+  //   const title = await driver.getTitle();
+  //   expect(title).to.equal("Netflix Login");
+  // });
 
-  it("Verifies if a user should not be allowed to log in with different credentials from the same browser at the same time.", async () => {
-    await login(driver);
-    // Try openning login page again
-    await driver.switchTo().newWindow("tab");
+  it("Verifies if the 'Remember Me' functionality keeps the user credentials after logout.", async () => {
+    // Login with remember me enabled
     await driver.get(BASE_URL);
-    // There is a small delay before showing the homepage
-    await new Promise((resolve) => setTimeout(() => resolve(true), 1000));
-    const title = await driver.getTitle();
-    expect(title).to.equal("Netflix Home");
+    await loginWithRememberMe(driver);
+    await driver.wait(until.elementLocated(By.id("logout")));
+
+    // Get local storage object
+    const oldStorage = await driver.executeScript(`return localStorage`);
+
+    // Quit current driver and create new one
+    await driver.quit();
+    driver = await buildDriver();
+
+    // Set the local storage of the new driver to include
+    // the username and password
+    await driver.get(BASE_URL);
+    await driver.executeScript("localStorage.setItem(arguments[0], arguments[1])", "Username", oldStorage["Username"]);
+    await driver.executeScript("localStorage.setItem(arguments[0], arguments[1])", "Password", oldStorage["Password"]);
+
+    await driver.wait(until.elementLocated(By.id("email_phone")));
+
+    const usernameValue = await driver.findElement(By.id("email_phone")).getAttribute("value");
+    const passwordValue = await driver.findElement(By.id("password")).getAttribute("value");
+
+    expect(usernameValue).to.equal(oldStorage["Username"]) && expect(passwordValue).to.equal(oldStorage["Password"]);
   });
 
   it("Verifies if a user should be able to login with the same credentials in different browsers at the same time.", async () => {
@@ -40,7 +56,6 @@ describe("TEST SUITE 3", () => {
     // Login with firefox
     const firefoxDriver = await new Builder().forBrowser("firefox").build();
     await login(firefoxDriver);
-
     // Check titles
     const chromeTitle = await driver.getTitle();
     const firefoxTitle = await firefoxDriver.getTitle();
@@ -48,6 +63,8 @@ describe("TEST SUITE 3", () => {
     firefoxDriver.quit();
   });
 });
+
+const buildDriver = async () => (driver = await new Builder().forBrowser("chrome").build());
 
 const login = async (driver) => {
   await driver.get(BASE_URL);
@@ -59,7 +76,17 @@ const login = async (driver) => {
   await driver.wait(until.elementLocated(By.id("logout")));
 };
 
-const logout = async (driver) => {
+const loginWithRememberMe = async (driver) => {
+  await driver.wait(until.elementLocated(By.id("email_phone")));
+  await driver.findElement(By.id("rememberMe")).click();
+  await driver.findElement(By.id("email_phone")).sendKeys("elham@gmail.com");
+  await driver.findElement(By.id("password")).sendKeys("elham123");
+  await driver.findElement(By.id("login-submit")).click();
+  // Wait to fully log in
   await driver.wait(until.elementLocated(By.id("logout")));
-  await driver.findElement(By.id("logout")).click();
 };
+
+// const logout = async (driver) => {
+//   await driver.wait(until.elementLocated(By.id("logout")));
+//   await driver.findElement(By.id("logout")).click();
+// };
